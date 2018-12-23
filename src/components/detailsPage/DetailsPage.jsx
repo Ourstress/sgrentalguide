5ae01db7291b17ede4e145615084643b99044c1c
SG <sgng1@e.ntu.edu.sg>
2018-12-23 02:17:02 +0000

    provided structure to detailsPage with viewer elements

--- src/components/detailsPage/DetailsPage.jsx
+++ src/components/detailsPage/DetailsPage.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import { Editor, Viewer } from "yosgo-editor";
 
-const DetailsPage = () => {
-  return <div>hi</div>;
+const DetailsPage = props => {
+  return (
+    <main>
+      <section>
+        <Viewer className="writeup" html={props.content.details} />
+      </section>
+      <section>
+        <article>
+          <header>Exercise</header>
+          <Viewer className="exercise" html={props.content.exercise} />
+          <Viewer className="answer" html={props.content.answer} />
+        </article>
+        <article>
+          <header>Resources & references</header>
+          <Viewer className="references" html={props.content.references} />
+        </article>
+      </section>
+    </main>
+    // <main>
+    //   <section>
+    //     <Editor className="writeup" />
+    //   </section>
+    //   <section>
+    //     <article>
+    //       <Editor className="exercise" />
+    //       <Editor className="answer" />
+    //     </article>
+    //     <article>
+    //       <Editor className="references" />
+    //     </article>
+    //   </section>
+    // </main>
+  );
 };
 
 export default DetailsPage;
--- src/data/rentalCollection.js
+++ src/data/rentalCollection.js
@@ -2,7 +2,13 @@ const rental = [
   {
     id: 1,
     name: "3 Checks under immigration act",
-    details: "blah"
+    details:
+      "<h2>3 Checks required under Immigration Act</h2><p>Under the law, homeowners intending to rent to foreigners are required to:</p><p>(a) Check his/her original immigration/work pass;&nbsp;</p><p>(b) Cross check the particulars on his/her pass against the particulars on his/her original passport; </p><p>(c) Verify the validity of his/her pass by checking with the issuing authority.</p> <p>If the homeowner </p><ul><li>carried out only one of the three due diligence checks, he may be sentenced to imprisonment for a term not less than six months and not more than two years and a fine not exceeding S$6,000.</li><li>carried out only two of the three due diligence checks, he may be sentenced to a fine not exceeding S$6,000 or to imprisonment for a term not exceeding 12 months or to both.</li></ul>",
+    exercise:
+      "<p>If the homeowner relied on the agent to check; the agent didn't check, and it was found out that the tenant is staying illegally; is the homeowner liable to penalties under the law?</p>",
+    answer: "Yes.",
+    references:
+      '<p><a href="http://www.nas.gov.sg/archivesonline/data/pdfdoc/20180226008/13%20Immigration%20Offenders%20Arrested%20In%20An%20Inland%20Operation.pdf" target="_blank">ICA news release 22Feb2018</a></p>'
   },
   {
     id: 2,

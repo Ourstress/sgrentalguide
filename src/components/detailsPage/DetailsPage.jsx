1ee7e5f0b24908f23d3c9c38e2047402f3df286d
SG <sgng1@e.ntu.edu.sg>
2018-12-27 11:05:33 +0000

    conditionally rendered editor in detailPage if editor logged in

--- src/components/detailsPage/DetailsPage.jsx
+++ src/components/detailsPage/DetailsPage.jsx
@@ -1,51 +1,67 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 import { Editor, Viewer } from "yosgo-editor";
 
 import styles from "./DetailsPage.module.css";
 
 const DetailsPage = props => {
+  const [notes, setNotes] = useState(props.content.Notes);
+  const [exercise, setExercise] = useState(props.content.Exercise);
+  const [answer, setAnswer] = useState(props.content.Answer);
+  const [references, setReferences] = useState(props.content.References);
+
   useEffect(() => {
     props.setSidebar(false);
   }, []);
+
   return (
     <article className={styles.detailsPageBody}>
       <section className={styles.writeup}>
-        <Viewer html={props.content.Notes} />
+        {props.user ? (
+          <Editor value={notes} onChange={setNotes} />
+        ) : (
+          <Viewer html={props.content.Notes} />
+        )}
       </section>
       <section className={styles.writeup}>
         <article>
           <header>
             <h2>Exercise</h2>
           </header>
-          <Viewer className="exercise" html={props.content.Exercise} />
+          {props.user ? (
+            <Editor
+              className="exercise"
+              value={exercise}
+              onChange={setExercise}
+            />
+          ) : (
+            <Viewer className="exercise" html={props.content.Exercise} />
+          )}
           <header>
             <h2>Answer</h2>
           </header>
-          <Viewer className="answer" html={props.content.Answer} />
+          {props.user ? (
+            <Editor className="answer" value={answer} onChange={setAnswer} />
+          ) : (
+            <Viewer className="answer" html={props.content.Answer} />
+          )}
         </article>
         <article>
           <header>
             <h2>Resources & references</h2>
           </header>
-          <Viewer className="references" html={props.content.References} />
+          {props.user ? (
+            <Editor
+              className="references"
+              value={references}
+              onChange={setReferences}
+            />
+          ) : (
+            <Viewer className="references" html={props.content.References} />
+          )}
         </article>
       </section>
     </article>
-    // <main>
-    //   <section>
-    //     <Editor className="writeup" />
-    //   </section>
-    //   <section>
-    //     <article>
-    //       <Editor className="exercise" />
-    //       <Editor className="answer" />
-    //     </article>
-    //     <article>
-    //       <Editor className="references" />
-    //     </article>
-    //   </section>
-    // </main>
   );
 };
 
